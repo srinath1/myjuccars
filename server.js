d0ee11f1c5f08efd8c07e1c67f13b223f62d5caf
Srinath N.S <srinath1@users.noreply.github.com>
2022-06-06 19:28:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const dbConnection = require('./db')
+const dbConnect = require("./dbConnect");
 app.use(express.json())
 
 app.use('/api/cars/' , require('./routes/carsRoute'))
